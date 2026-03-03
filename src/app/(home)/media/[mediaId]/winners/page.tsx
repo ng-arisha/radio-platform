06cfdb655e50c26fecdf755f9f35e028a56f41f9
Brian-mula <mulatiwangilabrian@gmail.com>
2026-03-03 17:29:20 +0300

    Correct spellings

--- src/app/(home)/media/[mediaId]/winners/page.tsx
+++ src/app/(home)/media/[mediaId]/winners/page.tsx
@@ -5,7 +5,7 @@ function WinnersPageForMediaHouses() {
         <div>
              <div className="mb-4">
             <h1 className="text-3xl font-medium text-white mb-2">💰 List of Winners</h1>
-            <p className="text-gray-400">Observe all winners within your medua house</p>
+            <p className="text-gray-400">Observe all winners within your media house</p>
           </div>
            <MediaHouseWinnersList />
         </div>
