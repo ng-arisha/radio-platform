42e9dcd0b78c5e5dfbe2841bd1e11d46d75369a9
Brian-mula <mulatiwangilabrian@gmail.com>
2026-03-10 14:38:22 +0300

    Fix rundraw

---
+++ src/app/(home)/shows/[showId]/run-draw/page.tsx
@@ -0,0 +1,13 @@
+import GenerateNewWinner from "@/components/finance/show/winners/generate-new-winner"
+import ShowNavigation from "@/components/shows/show-navigation"
+
+function RunDraw() {
+    return (
+        <div>
+             <ShowNavigation />
+             <GenerateNewWinner />
+        </div>
+    )
+}
+
+export default RunDraw
